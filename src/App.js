263374e7e03bcce53249bcf81b1a792de35dd5d3
Kolade-Junaid <koladejun@yahoo.com>
2020-01-21 16:57:09 +0000

    Modified the src/app.js file and finished the project git setup

--- src/App.js
+++ src/App.js
@@ -9,11 +9,18 @@ import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
 // import dummydata
 
+import "../../PostsPage.js";
+import "../..//dummy-data.js";
+
 const App = () => {
   // set up state for the dummy data and pass to your PostsPage
+  
   return (
     <div className="App">
       {/* Add components here  and pass props where appropriate */}
+
+
+      
     </div>
   );
 };
